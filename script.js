ca53e18e08b4f3256081c7d62ba053c3fee32570
Bradley Cardona <bradleymathematics@gmail.com>
2022-08-24 00:53:31 -0400

    add recursive depthFirstSearch function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // gameBoard must be an id -- it can't be a class. 
 let gameBoard = document.getElementById('gameBoard');
+let coordinateArray = []
 let numberOfRows = 27
 let numberOfCols = 64
 
@@ -11,6 +12,8 @@ function makeGrid(numRows, numCols) {
 
 for (let row = 0; row < numberOfRows; row++) {
     for (let col = 0; col < numberOfCols; col++) {
+        // add [row, col] to board array
+        coordinateArray.push([row, col])
         let div = document.createElement('div');
         div.id = `${row}-${col}`
         div.classList.add('unvisited_node');
@@ -20,12 +23,33 @@ for (let row = 0; row < numberOfRows; row++) {
 
 // Use querySelectorAll to retrieve direct children
 // Link: https://stackoverflow.com/questions/3680876/using-queryselectorall-to-retrieve-direct-children
-unvisitedNodes = gameBoard.querySelectorAll(":scope > .unvisited_node");
+let unvisitedNodes = gameBoard.querySelectorAll(":scope > .unvisited_node");
 unvisitedNodes.forEach(node => {
     node.addEventListener('click', () => {
-        node.style.backgroundColor = 'blue';
+      let coordinates = node.id.split("-");
+      let row = parseInt(coordinates[0]);
+      let col = parseInt(coordinates[1]);
+      let visitedNodes = []
+      for (let row = 0; row < numberOfRows; row++) {
+          for (let col = 0; col < numberOfCols; col++) {
+              visitedNodes.push([false]);
+          }
+      }
+      depthFirstSearch(row, col, visitedNodes);
     })
 });
 
+function depthFirstSearch(row, col, visitedNodes) {
+    if (row < 0 || row > 26 || col < 0 || col > 63 || visitedNodes[row][col] == true) {
+      return;
+    }
+    visitedNodes[row][col] = true;
+    let neighboringNode = document.getElementById(`${row}-${col}`);
+    neighboringNode.style.backgroundColor = 'purple';
+    setTimeout(depthFirstSearch, 10, row + 1, col, visitedNodes);
+    setTimeout(depthFirstSearch, 10, row - 1, col, visitedNodes);
+    setTimeout(depthFirstSearch, 10, row + 1, col + 1, visitedNodes);
+    setTimeout(depthFirstSearch, 10, row + 1, col - 1, visitedNodes);
+}
 
 makeGrid(numberOfRows, numberOfCols)  
