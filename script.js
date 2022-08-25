80fc67e1d890736bec61177bc9f69f94c4ea3772
Bradley Cardona <bradleymathematics@gmail.com>
2022-08-25 15:37:44 -0400

    add getNodeRow and getNodeColumn
    Additionally, I rearranged  code, and merged sections that were unnecessarily duplicated.

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.onmousedown = () => mouseDown = true;
 window.onmouseup = () => mouseDown = false;
 
 function makeGrid(numRows, numCols) {
-  gameBoard.style.setProperty('--numRows', numRows); 
-  gameBoard.style.setProperty('--numCols', numCols); 
+    gameBoard.style.setProperty('--numRows', numRows); 
+    gameBoard.style.setProperty('--numCols', numCols); 
 }
 
 // add "target" to one node, "unvisited" to other nodes
@@ -16,7 +16,6 @@ for (let row = 0; row < numberOfRows; row++) {
     for (let col = 0; col < numberOfCols; col++) {
         let div = document.createElement('div');
         div.id = `${row}-${col}`
-        // target node
         if (row == 13 && col == 55) {
             div.classList.add('target');
             gameBoard.appendChild(div);
@@ -37,144 +36,120 @@ function drawWall(e) {
   }
 }
 
+function getNodeRow(node) {
+    let coordinates = node.id.split("-");
+    let r = parseInt(coordinates[0]);
+    return (r);
+}
+
+function getNodeColumn(node) {
+    let coordinates = node.id.split("-");
+    let c = parseInt(coordinates[1]);  
+    return (c);
+}
+
 let unvisitedNodes = gameBoard.querySelectorAll(":scope > .unvisited");
 unvisitedNodes.forEach(node => {
     node.addEventListener('mousedown', drawWall);
     node.addEventListener('mouseover', drawWall);
     node.addEventListener('click', () => {
-        let coordinates = node.id.split("-");
-        let row = parseInt(coordinates[0]);
-        let col = parseInt(coordinates[1]);
-        // DFS
+        let row = getNodeRow(node); 
+        let col = getNodeColumn(node); 
         let currentNode = document.getElementById(`${row}-${col}`)
         DFS(currentNode);
     })
 });
 
-
 function DFS(node) {
-  let stack = [node]
-  let visitedNodesInOrder = new Set();
-  while (stack.length !== 0) {
-      currentNode = stack.pop();
-      visitedNodesInOrder.add(currentNode);
-      currentNode.classList.remove('unvisited');
-      currentNode.classList.add('visited');
-      let coordinates = currentNode.id.split("-");
-      let row = parseInt(coordinates[0]);
-      let col = parseInt(coordinates[1]);  
-      let possiblePathsForNode = 0;
-      let numberOfBarriers = 0; 
+    let stack = [node]
+    let visitedNodesInOrder = new Set();
+    while (stack.length !== 0) {
+        currentNode = stack.pop();
+        visitedNodesInOrder.add(currentNode);
+        currentNode.classList.remove('unvisited');
+        currentNode.classList.add('visited');
+        let possiblePathsForNode = 0;
+        let numberOfBarriers = 0; 
+        let row = getNodeRow(currentNode); 
+        let col = getNodeColumn(currentNode); 
 
-      function generalFunction(row, col) {
-        let node = document.getElementById(`${row}-${col}`);
-        if (node.classList.contains('visited') == false && node.classList.contains('wall') == false) {
-            stack.push(node);
-            possiblePathsForNode += 1;
-        } 
-        else if (node.classList.contains('wall')) {
-          numberOfBarriers += 1;
+
+        function generalFunction(row, col) {
+            let node = document.getElementById(`${row}-${col}`);
+            if (node.classList.contains('visited') == false && node.classList.contains('wall') == false) {
+                stack.push(node);
+                possiblePathsForNode += 1;
+            } 
+            else if (node.classList.contains('wall')) {
+                numberOfBarriers += 1;
+            }
         }
-      }
-      
-      if (currentNode.classList.contains('target')) {
-        break;
-      }
+        
+        if (currentNode.classList.contains('target')) {
+            break;
+        }
+
+        // leftNode, bottomNode, rightNode, topNode
+        (col - 1 >= 0) ? generalFunction(row, col - 1) : numberOfBarriers += 1;
+        (row + 1 <= 26) ? generalFunction(row + 1, col) : numberOfBarriers += 1;
+        (col + 1 <= 63) ? generalFunction(row, col + 1) : numberOfBarriers += 1;
+        (row - 1 >= 0) ? generalFunction(row - 1, col) : numberOfBarriers += 1;
+
+        if (possiblePathsForNode >= 2) {
+            currentNode.classList.add('fork');
+        }
+
+        if (numberOfBarriers >= 3) {
+            currentNode.classList.add('dead-end');
+        }
+    }
 
 
-      // leftNode
-      (col - 1 >= 0) ? generalFunction(row, col - 1) : numberOfBarriers += 1;
-      
-      // bottomNode
-      (row + 1 <= 26) ? generalFunction(row + 1, col) : numberOfBarriers += 1;
+    function getManhattanDistance(node) {
+        let row = getNodeRow(node); 
+        let col = getNodeColumn(node); 
+        let manhattanDistance = Math.abs(13 - row) + Math.abs(55 - col);
+        node.classList.add(`${manhattanDistance}`);
+        node.textContent = `${manhattanDistance}`
+    }
 
-      // rightNode
-      (col + 1 <= 63) ? generalFunction(row, col + 1) : numberOfBarriers += 1;
-      
-      // topNode
-      (row - 1 >= 0) ? generalFunction(row - 1, col) : numberOfBarriers += 1;
+    usefulNodes = [];
+    arrayOfVisitedNodes = Array.from(visitedNodesInOrder);
+    console.log(arrayOfVisitedNodes);
+    for (let i = 0; i < arrayOfVisitedNodes.length; i++) {
+        targetReached = false;
+        const node = arrayOfVisitedNodes[i];
+        if (node.classList.contains('target')) {
+            targetReached = true;
+            length = i;
+            for (let j = 0; j < length + 1; j++) {
+                setTimeout(() => {
+                    const pathToTargetNode = arrayOfVisitedNodes[j];
+                    pathToTargetNode.style.backgroundColor = 'blue';
+                }, 10 * j);
+            }
+        }
+    }
 
-
-      if (possiblePathsForNode >= 2) {
-        currentNode.classList.add('fork');
-      }
-
-      if (numberOfBarriers >= 3) {
-        currentNode.classList.add('dead-end');
-      }
-  }
-  
-  // Convert set to array
-  // Helpful link: https://stackoverflow.com/questions/16401216/iterate-over-set-elements
-
-  function getManhattanDistance(node) {
-    let coordinates = node.id.split("-");
-    let row = parseInt(coordinates[0]);
-    let col = parseInt(coordinates[1]);
-    let manhattanDistance = Math.abs(13 - row) + Math.abs(55 - col);
-    node.classList.add(`${manhattanDistance}`);
-    node.textContent = `${manhattanDistance}`
-    node.style.fontFamily = 'Lato';
-  }
-
-  d = [];
-
-// need to re-do this...
-  // label nodes between fork and dead-end as 'useless'
-  c = Array.from(visitedNodesInOrder);
-  c.reverse();
-  let addUseless = false;
-  for (let i = 0; i < c.length; i++) {
-      let node = c[i];
-      if (node.classList.contains('dead-end')) {
-          addUseless = true;
-      }
-      else if (node.classList.contains('fork')) {
-          addUseless = false;
-      }
-      if (addUseless) {
-          node.classList.add('useless')
-      }
-      else {
-          d.push(node);
-      }
-      console.log(node);
-      getManhattanDistance(node);
-  }
-
-  d.reverse()
-
-
-
-
-  b = Array.from(visitedNodesInOrder);
-  for (let i = 0; i < b.length; i++) {
-      targetReached = false;
-      const node = b[i];
-      // console.log(node);
-      if (node.classList.contains('target')) {
-          targetReached = true;
-          length = i;
-          let timeToFindNode = 0;
-          for (let j = 0; j < length + 1; j++) {
-              setTimeout(() => {
-                const pathToTargetNode = b[j];
-                pathToTargetNode.style.backgroundColor = 'blue';
-                // getManhattanDistance(pathToTargetNode);
-              }, 10 * j);
-              timeToFindNode = 10 * length + 1;
-          }
-          // setTimeout(() => {
-          //   for (let k = 0; k < d.length; k++) {
-          //       setTimeout(() => {
-          //           const pathToTargetNode = d[k];
-          //           pathToTargetNode.style.backgroundColor = 'yellow';
-          //       }, 10 * k);
-          //   }
-          // }, timeToFindNode);
-      }
-  }
-  return;
+    let addUseless = false;
+    for (let i = 0; i < arrayOfVisitedNodes.length; i++) {
+        let node = arrayOfVisitedNodes[i];
+        if (node.classList.contains('dead-end')) {
+            addUseless = true;
+        }
+        else if (node.classList.contains('fork')) {
+            addUseless = false;
+        }
+        if (addUseless) {
+            node.classList.add('useless')
+        }
+        else {
+            usefulNodes.push(node);
+        }
+        console.log(node);
+        getManhattanDistance(node);
+    }
+    return;
 }
 
 makeGrid(numberOfRows, numberOfCols) 
