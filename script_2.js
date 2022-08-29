a404251b18a9916afc7f4f74f5076f4c01f25d0c
Bradley Cardona <bradleymathematics@gmail.com>
2022-08-28 20:20:54 -0400

    delete comments; re-arrange code

--- script_2.js
+++ script_2.js
@@ -1,3 +1,85 @@
+
+class Node {
+  constructor(parent, row, col) {
+    this.parent = parent;
+    this.row = row;
+    this.col = col;
+    this.parents = []
+  }
+  addParent(name) {
+    this.parents.push(new Node(name));
+  }
+}
+
+// functions 
+function makeGrid(numRows, numCols) {
+  gameBoard.style.setProperty('--numRows', numRows); 
+  gameBoard.style.setProperty('--numCols', numCols); 
+}
+
+function drawWall(e) {
+if (mouseDown === true) {
+  e.target.style.backgroundColor = "black";
+  e.target.classList.remove('unvisited');
+  e.target.classList.add('wall');
+}
+}
+
+function getNodeRow(node) {
+  let coordinates = node.id.split("-");
+  let r = parseInt(coordinates[0]);
+  return (r);
+}
+
+function getNodeColumn(node) {
+  let coordinates = node.id.split("-");
+  let c = parseInt(coordinates[1]);  
+  return (c);
+}
+
+function changeNodeColor(node, color) {
+  node.style.backgroundColor = color;
+}
+
+function markNodeAsVisited(node) {
+  node.classList.remove('unvisited');
+  node.classList.add('visited');
+}
+
+function DFS(array, parentNode, row, col) {
+  let currentArray = array;
+  let nextNode = document.getElementById(`${row}-${col}`);
+  let childNode = new Node(parentNode, row, col);
+  childNode.addParent(nextNode);
+  if (row < 0 || row > 26 || col < 0 || col > 63 || nextNode.classList.contains('visited') || nextNode.classList.contains('wall')) {
+      return;
+  }
+  markNodeAsVisited(nextNode);
+
+  if (row == 13 && col == 55) {
+    targetReached = true;
+    console.log(childNode.parents)
+    let node = childNode;
+    while (node.parent != null) {
+        let shortestPathNode = document.getElementById(`${node.row}-${node.col}`);
+        shortestPath.push(shortestPathNode);
+        node = node.parent;
+    }
+  }
+
+  else if (targetReached == false) {
+    markNodeAsVisited(nextNode);
+    currentArray.push(nextNode);
+    DFS(currentArray, childNode, row + 1, col);
+    DFS(currentArray, childNode, row - 1, col);
+    DFS(currentArray, childNode, row, col + 1);
+    DFS(currentArray, childNode, row, col - 1);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
 // gameBoard must be an id -- it can't be a class. 
 let gameBoard = document.getElementById('gameBoard');
 let numberOfRows = 27
@@ -7,7 +89,6 @@ window.onmousedown = () => mouseDown = true;
 window.onmouseup = () => mouseDown = false;
 let shortestPath = []
 
-// add "target" to one node, "unvisited" to other nodes
 for (let row = 0; row < numberOfRows; row++) {
   for (let col = 0; col < numberOfCols; col++) {
       let div = document.createElement('div');
@@ -32,178 +113,37 @@ unvisitedNodes.forEach(node => {
       let row = getNodeRow(node); 
       let col = getNodeColumn(node); 
       let array = [];
-      // am I even the array back?
       DFS(array, null, row + 1, col)
       DFS(array, null, row - 1, col)
       DFS(array, null, row, col + 1)
       DFS(array, null, row, col - 1)
-      // DFS(startNode, row + 1, col, array);
-      // DFS(startNode, row - 1, col, array);
-      // DFS(startNode, row, col + 1, array);
-      // DFS(startNode, row, col - 1, array);
-      let timeToFinishBluePath = 20 * array.length;
+      let timeToFinishBluePath = 40 * array.length;
       for (let i = 0; i < array.length; i++) {
         setTimeout(() => {
             let node = array[i];
             changeNodeColor(node, 'slateblue');
             node.style.animation = "foundFirstPath 1s";
-        }, 20 * i); 
+        }, 40 * i); 
       }
       setTimeout(() => {
         shortestPath.reverse()
           for (let i = 0; i < shortestPath.length - 1; i++) {
             setTimeout(() => {
                 let node = shortestPath[i];
-                changeNodeColor(node, 'pink'); 
+                changeNodeColor(node, 'red'); 
                 node.style.animation = "foundShortestPath 1s";
-            }, 20 * i); 
+            }, 40 * i); 
           }
       }, timeToFinishBluePath)
   })
 });
 
-function makeGrid(numRows, numCols) {
-    gameBoard.style.setProperty('--numRows', numRows); 
-    gameBoard.style.setProperty('--numCols', numCols); 
-}
-
-function drawWall(e) {
-  if (mouseDown === true) {
-    e.target.style.backgroundColor = "black";
-    e.target.classList.remove('unvisited');
-    e.target.classList.add('wall');
-  }
-}
-
-function getNodeRow(node) {
-    let coordinates = node.id.split("-");
-    let r = parseInt(coordinates[0]);
-    return (r);
-}
-
-function getNodeColumn(node) {
-    let coordinates = node.id.split("-");
-    let c = parseInt(coordinates[1]);  
-    return (c);
-}
-
-function changeNodeColor(node, color) {
-    node.style.backgroundColor = color;
-}
-
-function markNodeAsVisited(node) {
-    node.classList.remove('unvisited');
-    node.classList.add('visited');
-}
 
 
-class Node {
-  constructor(parent, row, col) {
-    this.parent = parent;
-    this.row = row;
-    this.col = col;
-    this.parents = []
-  }
-  addParent(name) {
-    this.parents.push(new Node(name));
-  }
-}
+
 
 
 let targetReached = false;
-
-
-function DFS(array, parentNode, row, col) {
-    let currentArray = array;
-    let nextNode = document.getElementById(`${row}-${col}`);
-    let childNode = new Node(parentNode, row, col);
-    childNode.addParent(nextNode);
-    if (row < 0 || row > 26 || col < 0 || col > 63 || nextNode.classList.contains('visited') || nextNode.classList.contains('wall')) {
-        return;
-    }
-    markNodeAsVisited(nextNode);
-
-    if (row == 13 && col == 55) {
-      targetReached = true;
-      console.log(childNode.parents)
-      let node = childNode;
-      while (node.parent != null) {
-          let shortestPathNode = document.getElementById(`${node.row}-${node.col}`);
-          shortestPath.push(shortestPathNode);
-          node = node.parent;
-      }
-    }
-
-    else if (targetReached == false) {
-      markNodeAsVisited(nextNode);
-      currentArray.push(nextNode);
-      DFS(currentArray, childNode, row + 1, col);
-      DFS(currentArray, childNode, row - 1, col);
-      DFS(currentArray, childNode, row, col + 1);
-      DFS(currentArray, childNode, row, col - 1);
-    }
-}
-
-// function DFS(array, parentNode, row, col) {
-  //   let currentArray = array;
-  //   let nextNode = document.getElementById(`${row}-${col}`);
-  //   let childNode = new Node(parentNode, row, col);
-  //   childNode.addParent(nextNode);
-  //   if (row < 0 || row > 26 || col < 0 || col > 63 || nextNode.classList.contains('visited') || nextNode.classList.contains('wall')) {
-  //       return;
-  //   }
-  //   markNodeAsVisited(nextNode);
-  //   if (row == 13 && col == 55) {
-  //     targetReached = true;
-  //     console.log(childNode.parents)
-  //     let node = childNode;
-  //     while (node.parent != null) {
-  //         let shortestPathNode = document.getElementById(`${node.row}-${node.col}`);
-  //         shortestPathNode.style.backgroundColor = 'yellow';
-  //         node = node.parent;
-  //     }
-  //   }
-  
-  //   else {
-  //     markNodeAsVisited(nextNode);
-  //     currentArray.push(nextNode);
-  //     DFS(currentArray, childNode, row + 1, col);
-  //     DFS(currentArray, childNode, row - 1, col);
-  //     DFS(currentArray, childNode, row, col + 1);
-  //     DFS(currentArray, childNode, row, col - 1);
-  //   }
-  // }
-
-
-
-// function DFS(parentNode, row, col, array) {
-//     let currentArray = array
-//     let currentNode = document.getElementById(`${row}-${col}`);
-//     let childNode = new NodeParent(parentNode);
-//     if (row < 0 || row > 26 || col < 0 || col > 63 || currentNode.classList.contains('visited') || currentNode.classList.contains('wall')) {
-//         return;
-//     }
-//     markNodeAsVisited(currentNode);
-//     if (row == 13 && col == 55) {
-//       targetReached = true;
-//     }
-//     if (targetReached == false) {
-//       colorNodeBlue(currentNode);
-//       markNodeAsVisited(currentNode);
-//       currentArray.push(childNode);
-//     }
-//     // if (targetReached) {
-//     //   node = childNode;
-//     //   while (node.parent) {
-//     //       node.parent.style.backgroundColor = 'yellow';
-//     //       node = node.parent;
-//     //   }
-//     // }
-//     DFS(childNode, row + 1, col, currentArray);
-//     DFS(childNode, row - 1, col, currentArray);
-//     DFS(childNode, row, col + 1, currentArray);
-//     DFS(childNode, row, col - 1, currentArray);
-// }
 
 
 makeGrid(numberOfRows, numberOfCols) 
